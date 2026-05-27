41173a70f4be201bca97f9d210b5e6024e94ba9f
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-05-27 13:10:36 +0200

    añadir circulo decorativo a los titulos de las cards de Hero

--- oli-maleno/app/components/Hero.tsx
+++ oli-maleno/app/components/Hero.tsx
@@ -25,50 +25,59 @@ export const Hero = () => {
     const hoverState = isDesktop ? "hover" : "initial";
     return (
         <>
-            <motion.div initial='initial' whileHover={hoverState} className="w-11/12 h-[60dvh] md:w-1/2 md:h-[70dvh] bg-red-300 p-8 rounded-4xl">
-                <div className="h-10 overflow-hidden relative ">
-                    <motion.div
-                        variants={variantsText}
-                        transition={{ duration: 0.3, ease: "easeInOut" }}
-                        className="flex flex-col"
-                    >
-                        {/* Texto 1: El que se ve al principio */}
-                        <h2 className="text-4xl leading-none h-10 flex items-center">
-                            Productos
-                        </h2>
+            <motion.div initial='initial' whileHover={hoverState} className="w-11/12 h-[60dvh] md:w-1/2 md:h-[70dvh] bg-red-800/80 p-8 rounded-4xl">
+                <div className="flex items-center gap-3">
+                    <span className="h-4 w-4 rounded-full bg-white" /> {/* círculo */}
+                    <div className="h-10 overflow-hidden relative">
+                        <motion.div
+                            variants={variantsText}
+                            transition={{ duration: 0.3, ease: "easeInOut" }}
+                            className="flex flex-col"
+                        >
+                            {/* Texto 1: El que se ve al principio */}
+                            <h2 className="text-4xl leading-none text-white h-10 flex items-center">
+                                Productos
+                            </h2>
 
-                        {/* Texto 2: El que está escondido abajo esperando subir */}
-                        <h2 className="text-4xl leading-none h-10 flex items-center">
-                            Productos
-                        </h2>
-                    </motion.div>
+                            {/* Texto 2: El que está escondido abajo esperando subir */}
+                            <h2 className="text-4xl leading-none text-white h-10 flex items-center">
+                                Productos
+                            </h2>
+                        </motion.div>
+                    </div>
                 </div>
             </motion.div>
 
             <div className="gap-0 flex flex-col items-center md:w-1/2 md:gap-4 ">
-                <motion.div initial='initial' whileHover={hoverState} className="w-11/12 h-[40dvh] md:w-full md:h-[calc(35dvh-0.5rem)] bg-green-300 p-8 z-10 -mt-10 md:mt-0 rounded-4xl">
-                    <div className="h-10 overflow-hidden relative ">
-                        <motion.div
-                            variants={variantsText}
-                            transition={{ duration: 0.3, ease: "easeInOut" }}
-                            className="flex flex-col"
-                        >
-                            <h2 className="text-4xl leading-none h-10 flex items-center">Beneficios</h2>
-                            <h2 className="text-4xl leading-none h-10 flex items-center">Beneficios</h2>
-                        </motion.div>
+                <motion.div initial='initial' whileHover={hoverState} className="w-11/12 h-[40dvh] md:w-full md:h-[calc(35dvh-0.5rem)] bg-green-800/80 p-8 z-10 -mt-10 md:mt-0 rounded-4xl">
+                    <div className="flex items-center gap-3">
+                        <span className="h-4 w-4 rounded-full bg-white" /> {/* círculo */}
+                        <div className="h-10 overflow-hidden relative ">
+                            <motion.div
+                                variants={variantsText}
+                                transition={{ duration: 0.3, ease: "easeInOut" }}
+                                className="flex flex-col"
+                            >
+                                <h2 className="text-4xl leading-none text-white h-10 flex items-center">Beneficios</h2>
+                                <h2 className="text-4xl leading-none text-white h-10 flex items-center">Beneficios</h2>
+                            </motion.div>
+                        </div>
                     </div>
                 </motion.div>
 
-                <motion.div initial='initial' whileHover={hoverState} className="w-11/12 h-[40dvh] md:w-full md:h-[calc(35dvh-0.5rem)] bg-yellow-300 p-8 z-20 -mt-10 md:mt-0 rounded-4xl">
-                    <div className="h-20 md:h-10 overflow-hidden relative ">
-                        <motion.div
-                            variants={variantsText}
-                            transition={{ duration: 0.3, ease: "easeInOut" }}
-                            className="flex flex-col"
-                        >
-                            <h2 className="text-4xl leading-none h-20 md:h-10 flex items-center">Preguntas Frecuentes</h2>
-                            <h2 className="text-4xl leading-none h-20 md:h-10 flex items-center">Preguntas Frecuentes</h2>
-                        </motion.div>
+                <motion.div initial='initial' whileHover={hoverState} className="w-11/12 h-[40dvh] md:w-full md:h-[calc(35dvh-0.5rem)] bg-yellow-800/80 p-8 z-20 -mt-10 md:mt-0 rounded-4xl">
+                    <div className="flex items-center gap-3">
+                        <span className="h-4 w-4 rounded-full bg-white" /> {/* círculo */}
+                        <div className="h-20 md:h-10 overflow-hidden relative ">
+                            <motion.div
+                                variants={variantsText}
+                                transition={{ duration: 0.3, ease: "easeInOut" }}
+                                className="flex flex-col"
+                            >
+                                <h2 className="text-4xl leading-none text-white h-20 md:h-10 flex items-center">Preguntas Frecuentes</h2>
+                                <h2 className="text-4xl leading-none text-white h-20 md:h-10 flex items-center">Preguntas Frecuentes</h2>
+                            </motion.div>
+                        </div>
                     </div>
                 </motion.div>
             </div>
