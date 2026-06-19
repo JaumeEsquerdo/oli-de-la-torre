5c41a5e8f5cefccde78a3c08bd805f7e8cd03465
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-06-19 08:25:35 +0200

    actualizar diseño textos e introducir texto absoluto en imagen

--- oli-maleno/app/Nosotros/page.tsx
+++ oli-maleno/app/Nosotros/page.tsx
@@ -4,6 +4,7 @@ import { ShoppingCart } from "lucide-react";
 import { Menu } from "@/app/components/Menu";
 import { Footer } from '@/app/components/Footer';
 import { textNosotros } from '@/app/data/nosotros'
+import Image from 'next/image';
 
 
 const Nosotros = () => {
@@ -28,11 +29,14 @@ const Nosotros = () => {
                 </header>
                 <main className="flex flex-col gap-16 md:gap-36 flex-1 pt-8 md:mx-12 md:py-36">
                     <h1 className='flex flex-col gap-6'><span className='text-8xl text-[#666]'>SOBRE</span> <span className='text-8xl'>NOSOTROS</span></h1>
-                    <div className="flex flex-col md:gap-20 max-w-3/5 mx-auto">
-                        <div className='h-260 bg-amber-300 w-full'></div>
+                    <div className="flex flex-col md:gap-20 max-w-3/5 mx-auto items-center">
+                        <div className='h-260 bg-amber-800 w-full relative rounded-4xl fill-neutral-900'>
+                            {/* <Image src="" alt="" fill sizes='' /> */}
+                            <h2 className='absolute top-2/5 left-1/2 -translate-x-1/2 text-5xl w-[80%] text-white'>Más de 30 años cuidando nuestros olivos y manteniendo viva nuestra tradición familiar</h2>
+                        </div>
                         {textNosotros.filter(text => text.id === 1).map((text) => (
-                            <div key={text.id} className='flex flex-col gap-10'>
-                                <h2 className='text-2xl md:text-4xl'>{text.title}</h2>
+                            <div key={text.id} className='flex flex-col gap-10 w-[90%]'>
+                                <h3 className='text-2xl md:text-4xl font-semibold'>{text.title}</h3>
                                 <div className='flex flex-col gap-6'>
                                     <p>{text.p1}</p>
                                     <p>{text.p2}</p>
@@ -41,11 +45,11 @@ const Nosotros = () => {
                             </div>
                         ))}
                     </div>
-                    <div className="flex flex-col md:gap-20 max-w-3/5 mx-auto">
-                        <div className='h-140 bg-amber-600 w-full'></div>
+                    <div className="flex flex-col md:gap-20 max-w-3/5 mx-auto items-center">
+                        <div className='h-140 bg-amber-600 w-full rounded-4xl '></div>
                         {textNosotros.filter(text => text.id === 2).map((text) => (
-                            <div key={text.id} className='flex flex-col gap-10'>
-                                <h2 className='text-2xl md:text-4xl'>{text.title}</h2>
+                            <div key={text.id} className='flex flex-col gap-10  w-[90%]'>
+                                <h3 className='text-2xl md:text-4xl font-semibold'>{text.title}</h3>
                                 <div className='flex flex-col gap-6'>
                                     <p>{text.p1}</p>
                                     <p>{text.p2}</p>
