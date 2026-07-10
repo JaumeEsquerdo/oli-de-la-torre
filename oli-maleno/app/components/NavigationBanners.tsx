e6ba403ad9bbd6d29d523d30c1fb040aa8767436
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-07-10 16:54:03 +0200

    hacer los banners  responsive

--- oli-maleno/app/components/NavigationBanners.tsx
+++ oli-maleno/app/components/NavigationBanners.tsx
@@ -7,10 +7,10 @@ export const NavigationBanners = () => {
     // const [activeBanner, setActiveBanner] = useState<string | null>(null);
 
     return (
-        <div className="w-full flex mt-26 relative" >
+        <div className="w-full flex flex-col md:flex-row mt-26 relative" >
             {/* div para decorar los espacios q dejan los bordes redondeados cuando se juanta con el color del footer */}
             <div className="absolute bottom-0 h-80 right-0 w-full bg-white z-0" />
-            <div className=" group relative w-3/5 h-160 bg-green-500 rounded-4xl z-10 cursor-pointer"
+            <div className="group relative w-full md:w-3/5 h-160 bg-green-500 rounded-4xl z-10 cursor-pointer"
 
             >
                 <h3 className="text-4xl text-white absolute top-10 left-10 pointer-events-none">beneficios de nuestro aceite natural y
@@ -25,7 +25,7 @@ export const NavigationBanners = () => {
                     <ArrowUpRight className="w-6 h-6 text-green-500 stroke-[1.5]" />
                 </div>
             </div>
-            <div className="group relative w-2/5 h-160 bg-amber-400 rounded-4xl z-10 cursor-pointer"
+            <div className="group relative w-full md:w-2/5 h-160 bg-amber-400 rounded-4xl z-10 cursor-pointer"
 
             >
                 <h3 className="text-4xl text-white absolute top-10 left-10">preguntas y respuestas más habituales sobre el aceite y el envío</h3>
